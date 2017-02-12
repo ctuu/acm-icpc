7ab380af7afe67c911a18abc45e7ae223aec34c9
ctuu <ct.liuu@gmail.com>
2017-02-13 01:00:46 +0800

    commit

---
+++ 37-矩形面积交.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <iomanip>
+void swapnum(double &a, double &b);
+int main()
+{
+    double x1, x2, y1, y2;
+    double x3, x4, y3, y4;
+    double mx, lx, my, ly;
+    double tx, ty;
+    std::cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    swapnum(x1, x2);
+    swapnum(y1, y2);
+    swapnum(x3, x4);
+    swapnum(y3, y4);
+    tx = (x2 - x1) + (x4 - x3);
+    ty = (y2 - y1) + (y4 - y3);
+    mx = (x2 > x4) ? x2 : x4;
+    lx = (x1 < x3) ? x1 : x3;
+    my = (y2 > y4) ? y2 : y4;
+    ly = (y1 < y3) ? y1 : y3;
+    tx -= mx - lx;
+    ty -= my - ly;
+    if (tx <= 0 || ty <= 0)
+    std::cout << "0.00" << std::endl;
+    else
+    std::cout << std::fixed << std::setprecision(2) << tx * ty << std::endl;
+    return 0;
+}
+
+void swapnum(double &a, double &b)
+{
+    double temp;
+    if (a > b)
+    {
+        a = temp;
+        a = b;
+        b = temp;
+    }
+    return;
+}
--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,11 @@
- #include <iostream>
+#include <iostream>
 int main()
 {
-    std::cout << "Hello World." << std::endl;
+    int x1 = 3;
+    int x2 = 5;
+    x1 ^= x2;
+    x2 ^= x1;
+    x1 ^= x2;
+    std::cout<< "Hello World." << x1 << x2 << std::endl;
     return 0;
 }
