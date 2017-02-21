4a104f055d4956eb3de84b1706b434fcccfb5a67
ctuu <ct.liuu@gmail.com>
2017-02-21 20:55:27 +0800

    commit

--- TJPUOJ/39-完美的代价.cpp
+++ TJPUOJ/39-完美的代价.cpp
@@ -1,39 +1,60 @@
 #include <iostream>
+#define MAXCH 8001
+void swap(char *a, char *b);
+int tot;
 int main()
 {
-    bool ist = 0;
-    int n, i, te;
+    int n, i, isodd = 0, temp;
+    char *be, *end;
+    char oddch = ' ';
+    char tch;
     int cha[26] = {0};
-    char ch[8001];
+    char ch[MAXCH];
+
     std::cin >> n;
     for (i = 0; i < n; ++i)
     {
         std::cin >> ch[i];
-        te = ch[i] - 97;
-        ++cha[te];
+        ++cha[ch[i] - 97];
     }
-
     for (i = 0; i < 26; ++i)
-    {
-        std::cout << i << " " << cha[i] << std::endl;
-        if (!ist && cha[i] > 0)
+        if (cha[i] & 01)
         {
-            if (n & 01 && cha[i] & 01)
-            {
-                ist = 1;
-                te = i;
-            }
-            else if (!(n & 01) && cha[i] & 01)
-            {
-                std::cout << "Impossible";
-                return 0;
-            }
+            ++isodd;
+            oddch = i + 97;
         }
-        else if (cha[i] && cha[i] & 1)
+    if (isodd > 1)
+        std::cout << "Impossible";
+    else
+    {
+        be = ch;
+        end = ch + n - 1;
+        for (i = 0; i < n / 2; ++i, ++be)
         {
-            std::cout << "Impossible";
-            return 0;
+            if (*be == oddch)
+                for (temp = i; temp < n / 2; ++temp)
+                    swap(&ch[temp], &ch[temp + 1]);
+            else
+            {
+                for (temp = 0; *be != *end; ++temp)
+                    --end;
+                tch = *end;
+                std::cout << tch << std::endl;
+                for (; temp > 0; --temp, ++end)
+                    swap(end, end + 1);
+            }
         }
     }
+    for (i = 0; i < n; ++i)
+        std::cout << ch[i];
+    std::cout << tot;
     return 0;
+}
+
+void swap(char *a, char *b)
+{
+    char c = *a;
+    *a = *b;
+    *b = c;
+    ++tot;
 }
