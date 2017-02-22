2218dc946cafc6a000a042db7d905f8ea472c5b8
ctuu <ct.liuu@gmail.com>
2017-02-22 12:09:05 +0800

    commit

--- TJPUOJ/39-完美的代价.cpp
+++ TJPUOJ/39-完美的代价.cpp
@@ -1,63 +1,51 @@
-// 9
-// ffdejjell
-
 #include <iostream>
 #define MAXCH 8001
-void swap(char *a, char *b);
-int tot;
 int main()
 {
-    int n, i, isodd = 0, temp;
-    char *be, *end;
-    char oddch = ' ';
-    char tch;
-    int cha[26] = {0};
-    char ch[MAXCH];
-
-    std::cin >> n;
+    int n, i, isodd = 0;
+    int alph[26] = {0};
+    char ch[MAXCH], odd = '\0';
+    std::cin >> n >> ch;
     for (i = 0; i < n; ++i)
-    {
-        std::cin >> ch[i];
-        ++cha[ch[i] - 97];
-    }
+        ++alph[ch[i] - 97];
     for (i = 0; i < 26; ++i)
-        if (cha[i] & 01)
+        if (alph[i] & 01)
         {
             ++isodd;
-            oddch = i + 97;
+            odd = i + 97;
         }
     if (isodd > 1)
-        std::cout << "Impossible";
+        std::cout << "Impossible" << std::endl;
     else
     {
-        be = ch;
-        end = ch + n - 1;
-        for (i = 0; i < n / 2; ++i, ++be)
+        int change = 0;
+        for (i = 0; i < n / 2; ++i)
         {
-            if (*be == oddch && *be != ch[n - i - 1])
-                for (--be, temp = i; temp < n / 2; ++temp)
-                    swap(&ch[temp], &ch[temp + 1]);
+            if (ch[i] == odd)
+            {
+                // fdejjellf
+                int j = i;
+                for (; j <= n -i - 1; ++j)
+                    if (ch[j] == ch[n - 1 - i])
+                        break;
+                change += j - i;
+                for (int k = j; k > i; --k)
+                    ch[k] = ch[k - 1];
+                ch[i] = ch[n - 1 - i];
+            }
             else
             {
-                for (temp = 0; *be != *end; ++temp)
-                    --end;
-                for (; temp > 0; --temp, ++end)
-                    swap(end, end + 1);
-                --end;
-                for (temp = 0; temp < n; ++temp)
-                    std::cout << ch[temp];
-                std::cout << std::endl;
+                int j = 0;
+                for (j = n -i - 1; j > i; --j)
+                    if (ch[j] == ch[i])
+                        break;
+                change += n - 1 - j - i;
+                for (int k = j; k < n - 1 - i; ++k)
+                    ch[k] = ch[k + 1];
+                ch[n - 1 - i] = ch[i];
             }
         }
-        std::cout << tot;
+        std::cout << change;
     }
     return 0;
-}
-
-void swap(char *a, char *b)
-{
-    char c = *a;
-    *a = *b;
-    *b = c;
-    ++tot;
 }
--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,55 @@
-#include <iostream>
-#include <array>
-int main()
-{
-    std::array<int, 10> num;
-    for (auto &r:num)
-        std::cin >> r;
-    for (auto &r:num)
-    std::cout << r << " ";
-    return 0;
-}
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+    int N;     // 字符串长度  
+    cin >> N;  
+    char s[8001] = {0};  // 保证可存下结尾的'/0'  
+    cin >> s;  
+  
+    // 判断是否可变为回文串  
+    int b[26] = {0};   // 记录'a'~'z'出现的次数  
+    for (int i = 0; i <N; i++)  
+        b[s[i] - 'a']++;  // 相应字母出现次数加一  
+    int odd = 0;    // 有多少个字母出现奇数次  
+    char charodd = '\0';  // 出现奇数次的字母  
+    for (int i = 0; i <26; i++)  
+        if (b[i] % 2 == 1)  // b[i] 是奇数  
+        {  
+            odd++;  
+            charodd = i + 'a'; // 记录该字母  
+        }  
+    if (odd> 1)// 不可变为回文串（当 odd 为 0 或 1 时，都可以）  
+        cout << "Impossible" << endl;  
+    else  
+    {  
+        int change = 0;     // 交换次数  
+        for (int i = 0; i <N/2; i++) // 依次考虑中点左侧的字母  
+        {  
+            if (s[i] == charodd)  // 是 charodd，转而考虑右侧字母  
+            {  
+                int j = 0;  
+                for (j = i; j <= N-i-1; j++) // 从左侧该位置开始，向右直至右侧对称位置，找相同字母  
+                    if (s[j] == s[N-i-1])  // 找到  
+                        break;  
+                change += j - i;    // 需要 j-i 次移动可到左侧位置  
+                for (int k = j; k > i; k--)  // 实现字母的移动  
+                    s[k] = s[k-1];  
+                s[i] = s[N-i-1];// 为了省事，并非两两互换，只是右边赋为左边的值，所以最后将 i 位赋值  
+            }  
+            else  // 考虑左侧字母  
+            {  
+                int j = 0;  
+                for (j = N-i-1; j>= i; j--) // 从右侧对称位置开始，向左直至 i 位（向右是排序好了的），找相同字母  
+                    if (s[j] == s[i])   // 找到  
+                        break;  
+                change += N-i-1 - j;   // 需要 N-i-j-j 次移动可到右侧位置  
+                for (int k = j; k <N-i-1; k++) // 实现字母的移动  
+                    s[k] = s[k+1];  
+                s[N-i-1] = s[i];  
+            }  
+        }  
+        cout << change << endl;     // 输出  
+    }  
+    return 0;  
+}  
