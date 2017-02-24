8685fe0941d4729eb8e9533f1e2f8077f3d04764
ctuu <ct.liuu@gmail.com>
2017-02-24 18:26:30 +0800

    commit

---
+++ TJPUOJ/47-传球游戏.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+int main()
+{
+    int pass[32][32] = {0};
+    int n, m, i, j;
+    std::cin >> n >> m;
+    pass[0][0] = 1;
+    for (i = 0; i <= m; ++i)
+        for (j = 0; j < n; ++j)
+        { 
+            if (pass[i][j] > 0)
+            {
+                if (j == 0)
+                {
+                    pass[i + 1][1] += 1;
+                    pass[i + 1][n - 1] +=1;
+                }
+                else if (j == n - 1)
+                {
+                    pass[i + 1][0] +=1;
+                    pass[i + 1][n - 2] += 1;
+                }
+                else
+                {
+                    pass[i + 1][j + 1] +=1;
+                    pass[i + 1][j - 1] +=1;
+                }
+            }
+        }
+    std::cout << pass[m][0] << std::endl;
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            std::cout << pass[i][j] << " ";
+        std::cout << std::endl;
+    }
+    return 0;
+}
