d2124f849b8c8b203090e36a6e08067fbb9919a9
ctuu <ct.liuu@gmail.com>
2017-02-28 10:10:23 +0800

    commit

--- TJPUOJ/48-蚂蚁感冒.cpp
+++ TJPUOJ/48-蚂蚁感冒.cpp
@@ -1,72 +1,39 @@
 #include <iostream>
 int main()
 {
-    int n, i, temp, fir, sick = 1;
-    int road[101];
+    int n, i, sick = 0, one;
+    int num[52];
     std::cin >> n;
     for (i = 0; i < n; ++i)
+        std::cin >> num[i];
+    one = num[0];
+    for (i = 1; i < n; ++i)
     {
-        std::cin >> temp;
-        if (i == 0)
-            fir = temp;
-        if (temp > 0)
-            road[temp] = 1;
-        else
-            road[temp] = -1;
-    }
-    if (road[fir] > 0)
-        ++road[fir];
-    else
-        --road[fir];
-
-    while (n)
-        for (i = 0; i < 101; ++i)
+        if (one > 0)
         {
-            if (road[i] > 0)
-            {
-                if (road[i + 1] == 0)
-                {
-                    ++road[i + 1];
-                    --road[i];
-                }
-                else if (road[i + 1] == 1 || road[i + 1] == -1)
-                {
-                    road[i + 1] = -road[i + 1];
-                    road[i] = -road[i];
-                }
-                else if (road[i + 1] == 2 || road[i + 1] == -2)
-                {
-                    road[i + 1] = -road[i + 1];
-                    road[i] = -road[i];
-                    ++sick;
-                }
-            }
-
-            else if (road[i] < 0)
-            {
-                if (road[i - 1] == 0)
-                {
-                    --road[i - 1];
-                    ++road[i];
-                }
-                else if (road[i - 1] == 1 || road[i - 1] == -1)
-                {
-                    road[i - 1] = -road[i - 1];
-                    road[i] = -road[i];
-                }
-                else if (road[i - 1] == 2 || road[i - 1] == -2)
-                {
-                    road[i - 1] = -road[i - 1];
-                    road[i] = -road[i];
-                    ++sick;
-                }
-            }
-            if (i == 0 || i == 101)
-            {
-                road[i] = 0;
-                --n;
-            }
+            if (num[i] < 0 && -num[i] > one)
+                ++sick;
+            if (num[i] > 0 && num[i] < one)
+                ++sick;
         }
+        else
+        {
+            if (num[i] > 0 && num[i] < -one)
+                ++sick;
+                if (num[i] < 0 && num[i] < one)
+                ++sick;
+        }
+    }
+    // for (i = 1; sick > 0 && i < n; ++i)
+    // {
+    //     if (one > 0)
+    //     {
+    //     }
+    //     else
+    //     {
+            
+    //     }
+    // }
     std::cout << sick;
     return 0;
 }
