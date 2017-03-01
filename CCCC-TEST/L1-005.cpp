d9aaeeca79ddff11bcdef28970f81e3bd2119bf5
ctuu <ct.liuu@gmail.com>
2017-03-01 23:04:03 +0800

    commit

--- CCCC-TEST/L1-005.cpp
+++ CCCC-TEST/L1-005.cpp
@@ -3,6 +3,23 @@
 #include <vector>
 int main()
 {
-    std::vector<std::string> no(10);
+    std::vector<std::string> no(1001);
+    std::string temch;
+    int i, n, temp;
+    int test[1001] = {0};
+    std::cin >> n;
+    while (n--)
+    {
+        std::cin >> temch >> temp;
+        std::cin >> i;
+        test[temp] = i;
+        no[i] = temch;
+    }
+    std::cin >> n;
+    while (n--)
+    {
+        std::cin >> i;
+        std::cout << no[test[i]] << " " << test[i] << std::endl;
+    }
     return 0;
 }
