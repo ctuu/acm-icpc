eb366a1cf01c89c317625650833536858d469f77
ctuu <ct.liuu@gmail.com>
2017-03-04 23:51:21 +0800

    commit

--- CCCC-TEST/L1-009.cpp
+++ CCCC-TEST/L1-009.cpp
@@ -1,2 +1,63 @@
 #include <iostream>
+long long GCD(long long a,  long long b);
+long long LCM(long long a, long long b);
+struct dfra
+{
+    long long mum;
+    long long den;
+};
 
+struct calcfra
+{
+    dfra fra;
+    dfra& combine(dfra rhs);
+};
+
+dfra& calcfra::combine(dfra rhs)
+{
+    int i = LCM(fra.den, rhs.den);
+    fra.mum *= i / fra.den;
+    rhs.mum *= i / rhs.den;
+    fra.den = i;
+    fra.mum += rhs.mum;
+    i = GCD(fra.mum, fra.den);
+    fra.mum /= i;
+    fra.den /= i;
+    return this->fra;
+}
+
+
+int main()
+{
+    int i, mixn;
+    calcfra now;
+    dfra next;
+    char t;
+    std::cin >> i >> now.fra.mum >>  t >> now.fra.den;
+    while (--i)
+    {
+        std::cin >> next.mum  >> t >> next.den;
+        now.combine(next);
+    }
+    mixn = now.fra.mum / now.fra.den;
+    if (mixn)
+        std::cout << mixn;
+    now.fra.mum -= mixn * now.fra.den;
+    if (now.fra.mum)
+    {
+        if (mixn)
+            std::cout << " ";
+        std::cout << now.fra.mum << "/" << now.fra.den;
+    }
+    return 0;
+}
+
+long long GCD(long long a,  long long b)
+{
+    if (b) while ((a %= b) && (b %= a));
+    return a + b;
+}
+long long LCM(long long a, long long b)
+{
+    return a * b / GCD(a, b);
+}
