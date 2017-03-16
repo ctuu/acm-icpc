5c8472e54fee7c5af436ba86f2766582028ac2d6
ctuu <ct.liuu@gmail.com>
2017-03-16 19:44:19 +0800

    commit

--- CCCC-TEST/L2-008.cpp
+++ CCCC-TEST/L2-008.cpp
@@ -6,14 +6,17 @@ int main()
 {
     std::string word;
     std::array<int, 3002> len;
+    len.fill(0);
     std::getline(std::cin, word);
-    std::cout << word.size() << std::endl;
     int id = word.size() + 1;
     for (int i = 0; id--; i += 2)
         word.insert(i, 1, sch);
     id = 0;
     int maxlen = 0;
-    std::cout << word << std::endl;
+    for (int i = 0; i < word.size(); ++i)
+        std::cout << word[i];
+    std::cout << std::endl;
+    // std::cout << word << std::endl;
     for (int i = 1; i < word.size(); ++i)
     {
         if (len[id] + id > i)
