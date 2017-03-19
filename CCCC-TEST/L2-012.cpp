fd763cbff452781cf2ffa2645c46c9e958c4d26b
ctuu <ct.liuu@gmail.com>
2017-03-19 20:54:29 +0800

    commit

--- CCCC-TEST/L2-012.cpp
+++ CCCC-TEST/L2-012.cpp
@@ -2,14 +2,14 @@
 #include <algorithm>
 #include <vector>
 #include <string>
-std::vector <int> heap;
+std::vector<int> heap;
 int sfind(int i);
 int main()
 {
     int n, m, num;
     std::cin >> n >> m;
     heap.resize(n);
-    for (auto& i : heap)
+    for (auto &i : heap)
         std::cin >> i;
     std::make_heap(heap.begin(), heap.end(), std::greater<int>());
     while (m--)
@@ -20,16 +20,40 @@ int main()
         std::cin >> i >> word;
         if (word == "and")
         {
-            std::cout << "1";
             std::cin >> j;
-            if (sfind(i) / 2 == sfind(j) / 2)
+            if ((sfind(i) + 1) / 2 == (sfind(j) + 1) / 2)
                 flag = 1;
+            std::cin >> word >> word;
+        }
+        else
+        {
+            std::cin >> word;
+            if (word == "a")
+            {
+                std::cin >> word >> word >> j;
+                if ((sfind(i) - 1) / 2 == sfind(j))
+                    flag = 1;
+            }
+            else
+            {
+                std::cin >> word;
+                if (word == "root")
+                {
+                    if (sfind(i) == heap[0])
+                        flag = 1;
+                }
+                else
+                {
+                    std::cin >> word >> j;
+                    if (sfind(i) == (sfind(j) + 1) / 2)
+                        flag = 1;
+                }
+            }
         }
         if (flag)
             std::cout << "T" << std::endl;
         else
             std::cout << "F" << std::endl;
-        // std::cout << sfind(i);
     }
     return 0;
 }
