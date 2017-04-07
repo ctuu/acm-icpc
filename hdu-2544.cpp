7546ccca799bd077d0e147944e46b4a6b42ff6b7
ctuu <ct.liuu@gmail.com>
2017-04-07 09:44:26 +0800

    commit

--- hdu-2544.cpp
+++ hdu-2544.cpp
@@ -1,9 +1,53 @@
 #include <bits/stdc++.h>
+#define MNUM 102
+#define INF 0x03ffffff
 using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    int n, m;
+    while (cin >> n >> m && n && m)
+    {
+        array<array<int, MNUM>, MNUM> lj;
+        array<int, MNUM> rlj;
+        rlj.fill(INF);
+        for (auto &i : lj)
+            i.fill(0);
+        rlj[1] = 0;
+        while (m--)
+        {
+            int a, b, c;
+            cin >> a >> b >> c;
+            if (lj[a][b])
+                lj[a][b] = lj[b][a] = min(c, lj[a][b]);
+            else
+                lj[a][b] = lj[b][a] = c;
+        }
 
+        int cur = 1;
+        while (cur < n)
+        {
+            queue<int> mod;
+            for (int i = 1; i <= n; ++i)
+            {
+                if (lj[cur][i])
+                {
+                    mod.push(i);
+                    rlj[i] = min(rlj[i], rlj[cur] + lj[cur][i]);
+                    lj[cur][i] = lj[i][cur] = 0;
+                }
+            }
+            int next = mod.front();
+            while (!mod.empty())
+            {
+                int i = mod.front();
+                next = (rlj[next] < rlj[i]) ? next : i;
+                mod.pop();
+            }
+            cur = next;
+        }
+        cout << rlj[n] << endl;
+    }
     return 0;
 }
--- hello.cpp
+++ hello.cpp
@@ -1,29 +1,68 @@
-#include <bits/stdc++.h>
-using namespace std;
-struct lrs {
-    string cn;
-    string en;
-};
-bool comp(lrs a, lrs b);
+#include <stdio.h>
+
+#define MAX 0x3fffffff
+int map[105][105], dis[105];
+bool use[105];
+int n, m;
+
+void dijk(int start)
+{
+    int min, rj, i, j;
+    for (j = 1; j <= n; j++)
+    {
+        dis[j] = map[1][j];
+        use[j] = 0;
+    }
+    use[1] = 1;
+    for (i = 1; i < n; i++)
+    {
+        min = MAX;
+        for (j = 1; j <= n; j++)
+        {
+            if (use[j] == 1)
+                continue;
+            if (dis[j] < min)
+            {
+                min = dis[j];
+                rj = j;
+            }
+        }
+
+        use[rj] = 1;
+        for (j = 1; j <= n; j++)
+        {
+            if (use[j] == 1)
+                continue;
+            if (dis[j] > dis[rj] + map[rj][j])
+                dis[j] = dis[rj] + map[rj][j];
+        }
+    }
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    vector<lrs> word;
-    int i;
-    cin >> i;
-    word.resize(i);
-    for (auto &c:word)
-        cin >> c.cn >> c.en;
-    sort(word.begin(), word.end(),comp);
-    cout << endl << "Result" << endl;
-    for (auto c:word)
-    cout << setw(6) << fixed << c.cn << " " << c.en << endl;
-    return 0;
-}
-bool comp(lrs a, lrs b)
-{
-    if (a.en <= b.en)
-        return 1;
-    return 0;
+    int i, j, a, b, temp;
+    while (scanf("%d%d", &n, &m), n || m)
+    {
+        for (i = 1; i <= n; i++)
+        {
+            for (j = 1; j <= n; j++)
+            {
+                map[i][j] = MAX;
+            }
+        }
+        for (i = 1; i <= m; i++)
+        {
+            scanf("%d%d", &a, &b);
+            scanf("%d", &temp);
+            if (temp < map[a][b])
+            {
+                map[b][a] = temp;
+                map[a][b] = temp;
+            }
+        }
+
+        dijk(1);
+        printf("%d\n", dis[n]);
+    }
 }
