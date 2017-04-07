33d4c0899fd64e3febc8b178732c4bf8be99b0cc
ctuu <ct.liuu@gmail.com>
2017-04-07 18:01:57 +0800

    commit

--- hdu-1863.cpp
+++ hdu-1863.cpp
@@ -1,10 +1,64 @@
 #include <bits/stdc++.h>
+#define MNUM 102
+#define INF 0x03ffffff
 using namespace std;
+
+struct path {
+    int a, b, cost;
+};
+struct comp_path{
+	bool operator()(const path i, const path j) const
+	{
+		return i < j;
+	}
+};
+
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int n, m;
+    cin >> n >> m;
+    array<array<int, MNUM>, MNUM> lj;
+    array<int, MNUM> rlj;
     
+    rlj.fill(INF);
+    for (auto &i: lj)
+        i.fill(0);
+    rlj[1] = 0;
+    
+    while (m--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (lj[a][b])
+            lj[a][b] = lj[b][a] = min(c, lj[a][b]);
+        else
+            lj[a][b] = lj[b][a] = c;
+    }
+
+    int cur = 1;
+    int tot = 0;
+    set<int> vied;
+    vied.insert(1);
+    while (tot < n)
+    {
+        set<path, comp_path> sel;
+        for (auto i: vied)
+        {
+            path temp;
+            for (j = 1; j <= n; ++j)
+                if(lj[i][j])
+                {
+                    temp.a = i;
+                    temp.b = j;
+                    temp.cost = lj[i][j];
+                    sel.insert(temp);
+                }
+        }
+        cur = next;
+        ++tot;
+    }
+    cout << elj[n] << endl;
     return 0;
 }
--- hello.cpp
+++ hello.cpp
@@ -1,68 +1,35 @@
-#include <stdio.h>
-
-#define MAX 0x3fffffff
-int map[105][105], dis[105];
-bool use[105];
-int n, m;
-
-void dijk(int start)
-{
-    int min, rj, i, j;
-    for (j = 1; j <= n; j++)
-    {
-        dis[j] = map[1][j];
-        use[j] = 0;
-    }
-    use[1] = 1;
-    for (i = 1; i < n; i++)
-    {
-        min = MAX;
-        for (j = 1; j <= n; j++)
-        {
-            if (use[j] == 1)
-                continue;
-            if (dis[j] < min)
-            {
-                min = dis[j];
-                rj = j;
-            }
-        }
-
-        use[rj] = 1;
-        for (j = 1; j <= n; j++)
-        {
-            if (use[j] == 1)
-                continue;
-            if (dis[j] > dis[rj] + map[rj][j])
-                dis[j] = dis[rj] + map[rj][j];
-        }
-    }
-}
-
+#include <bits/stdc++.h>
+using namespace std;
+struct path{
+	int a;
+	int b;
+ 
+};
+struct comp {
+	bool operator()(const path i, const path j) const
+	{
+	return i.b < j.b;
+	}
+};
 int main()
 {
-    int i, j, a, b, temp;
-    while (scanf("%d%d", &n, &m), n || m)
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    set<path, comp> road;
+    int k = 4;
+    while (k--)
     {
-        for (i = 1; i <= n; i++)
-        {
-            for (j = 1; j <= n; j++)
-            {
-                map[i][j] = MAX;
-            }
-        }
-        for (i = 1; i <= m; i++)
-        {
-            scanf("%d%d", &a, &b);
-            scanf("%d", &temp);
-            if (temp < map[a][b])
-            {
-                map[b][a] = temp;
-                map[a][b] = temp;
-            }
-        }
+    	path temp;
+		cin >> temp.a >> temp.b;
+		road.insert(temp);
+	}
+	for (auto i: road)
+	cout << i.a << " " << i.b << endl;
+    cout << "Hello";
+    return 0;
+}
 
-        dijk(1);
-        printf("%d\n", dis[n]);
-    }
+bool comp(path i, path j)
+{
+	return i.b < j.b;
 }
