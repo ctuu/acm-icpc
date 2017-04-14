6798b9594cb43f7c049cf8f17422ce2a629b4555
ctuu <ct.liuu@gmail.com>
2017-04-14 10:12:09 +0800

    commit

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,1 +1,39 @@
 // 邻接表
+#include <bits/stdc++.h>
+#define INF 0x0FFFFFFF
+using namespace std;
+
+struct edge {
+    int to;
+    int v = INF;
+};
+struct comp_path{
+    bool operator()(const edge i, const edge j) const {return i.to > j.to;}
+};
+int dij(vector<vector<edge>> &lj, int be, int en);
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int no, path, be, en;
+    vector<vector<edge>> lj;
+    cin >> no >> path >> be >> en;
+    lj.resize(no);
+    for (int i = 0; i < path; ++i)
+    {
+        int a;
+        edge cur;
+        cin >> a >> cur.to >> cur.v;
+        lj[i].push_back(cur);
+    }
+    cout << dij(lj, be, en);
+    return 0;
+}
+int dij(vector<vector<edge>> &lj, int be, int en)
+{
+    int n = lj.size();
+    vector<int> rlj(n, INF);
+    set<edge> mod;
+    return 0;
+}
--- hello.cpp
+++ hello.cpp
@@ -1,50 +1,6 @@
 #include <iostream>
-#include <algorithm>
-#include <cstdio>
-#define inf 0x7fffffff
-using namespace std;
-int n, p, c[10001], ft[10001], ans = inf, tot;
-struct data
-{
-    int x, y, v;
-} e[100001];
-bool cmp(data a, data b)
-{
-    return a.v < b.v;
-}
-int find(int x) { return x == ft[x] ? x : ft[x] = find(ft[x]); }
 int main()
 {
-        scanf("%d%d", &n, &p);
-    for (int i = 1; i <= n; i++)
-    {
-	       scanf("%d", &c[i]);
-	       ans = min(ans, c[i]);
-	       ft[i] = i;
-    }
-    for (int i = 1; i <= p; i++)
-    
-    {
-	     int u, v, w;
-	     scanf("%d%d%d", &u, &v, &w);
-	     w = (w << 1) + c[u] + c[v];
-	        e[i].x = u;
-	e[i].y = v;
-	e[i].v = w;
-    }
-    sort(e + 1, e + p + 1, cmp);
-    for (int i = 1; i <= p; i++)
-    {
-		int p = find(e[i].x), q = find(e[i].y);
-		if (p != q)
-		{
-			ft[p] = q;
-			tot++;
-			ans += e[i].v;
-		}
-		if (tot == n - 1)
-			break;
-    }
-    printf("%d", ans);
+    std::cout << "Hello World.";
     return 0;
 }
