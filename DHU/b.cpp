bd3ea2e2b91b56c601658af8bd26af8cb0c950fd
ctuu <ct.liuu@gmail.com>
2017-06-01 19:52:21 +0800

    commit

--- DHU/b.cpp
+++ DHU/b.cpp
@@ -1,31 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 int fa[5002] = {0};
-void find(int x);
+int find(int x);
+void merge(int x, int y);
 int main()
 {
     int n, m;
     while (cin >> n >> m)
     {
-        int re[5002] = {0};
         for (int i = 0; i <= n; ++i)
             fa[i] = i;
         for (int i = 0; i < m; ++i)
         {
             int a, b;
             cin >> a >> b;
-            fa[a] = b;
+            merge(a, b);
         }
+        int re[5002] = {0};
         for (int i = 1; i <= n; ++i)
         {
-            find(i);
-            ++re[fa[i]];
-            cout << i << "to" << fa[i] << endl;
+            ++re[find(i)];
+            //cout << i << "to" << fa[i] << endl;
         }
         bool fl = 1;
         for (int i = 1; i <= n; ++i)
         {
-            cout << i << ":" << re[i] << endl;
+            // cout << i << ":" << re[i] << endl;
             if (re[i] > 0 && re[i] % 3 != 0)
                 fl = 0;
         }
@@ -37,7 +37,7 @@ int main()
     return 0;
 }
 
-void find(int x)
+int find(int x)
 {
     int a = x;
     while (fa[a] != a)
@@ -50,5 +50,13 @@ void find(int x)
         fa[i] = a;
         i = te;
     }
+    return a;
+}
+
+void merge(int x, int y)
+{
+    int fx = find(x), fy = find(y);
+    if (fx != fy)
+       fa[fx] = fy;
     return;
 }
