acb1f07237eb17fe2ce460c5e48fc52d65782c5a
ctuu <ct.liuu@gmail.com>
2017-07-09 14:06:02 +0800

    commit

---
+++ summer_2017_shu/a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    array<int, 102> a;
+    int m, n, k;
+    while (cin >> m >> n)
+    {
+        a.fill(0);
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (cin >> k, k == 1)
+                    ++a[j];
+        for (int i = 0; i < n; ++i)
+            cout << a[i] << ' ';
+        cout << endl;
+    }
+    return 0;
+}
---
+++ summer_2017_shu/c.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+// #include <cctype>
+using namespace std;
+int main()
+{
+    // ios_base::sync_with_stdio(0);
+    // cin.tie(0);
+    string text;
+    array<int , 27> a;
+    while (cin >> text)
+    {
+        a.fill(0);
+        // for (auto i :text)
+        //     ++a[toupper(i) - 'A'];
+        for (auto i: a)
+            cout << i << ' ';
+    }
+    
+    return 0;
+}
