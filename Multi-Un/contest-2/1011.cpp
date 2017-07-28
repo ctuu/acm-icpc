515386e2a7bcdabd1fe5c738f7e93ae55456b7d2
ctuu <ct.liuu@gmail.com>
2017-07-28 14:26:10 +0800

    commit

--- Multi-Un/contest-2/1011.cpp
+++ Multi-Un/contest-2/1011.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 class po
 {
-  private:
-    int x, y;
-
   public:
+    int x, y;
+    po() = default;
+    po(int a, int b)
+    {
+        this->x = a;
+        this->y = b;
+    }
     bool operator<(class po const &b) const
     {
         if (x == b.x)
@@ -27,24 +31,37 @@ class po
     }
     friend istream &operator>>(istream &is, po &item)
     {
-        is >>item.x >> item.y;
+        is >> item.x >> item.y;
         if (is)
-        ;
+            ;
         else
             item = po();
         return is;
     }
-    friend ostream &operator << (ostream &os, const po &item)
+    friend ostream &operator<<(ostream &os, const po &item)
     {
         os << "x: " << item.x << " y: " << item.y << endl;
         return os;
     }
-    // bool issq(class po const &a)
-    // {
-    //     po pc, pd;
-    //     return true;
-    //     return false;
-    // }
+    void poly(class po const &a, class po const &b, bool fos)
+    {
+        if (fos == false)
+        {
+            this->y = a.y + b.x - a.x;
+            if (a.x >= b.x)
+                this->x = a.x + a.y - b.y;
+            else
+                this->x = a.x + b.y - a.y;
+        }
+        else
+        {
+            this->y = b.y + b.x - a.x;
+            if (a.x >= b.x)
+                this->x = b.x + a.y - b.y;
+            else
+                this->x = b.x + b.y - a.y;
+        }
+    }
 };
 
 int main()
@@ -63,17 +80,38 @@ int main()
             cin >> temp;
             oc.insert(temp);
         }
-        for (auto it = oc.begin(); it != oc.end(); ++it)
-            cout << *it;
-        // for (set<po>::iterator it = oc.begin(); it != oc.end(); ++it)
-        // for (auto i : oc)
-        // {
-        //     for (auto j : oc)
-        //         if (i == j)
-        //             continue;
-        //         else
-        //             cout << "2";
-        // }
+
+        // for (auto it = oc.cbegin(); it != oc.cend(); ++it)
+        //     cout << *it;
+        for (auto i = oc.cbegin(); i != oc.cend(); ++i)
+            for (auto j = i; j != oc.cend(); ++j)
+            {
+                if (i == j)
+                    continue;
+                po pc, pd;
+                bool ti = 1;
+                if (i->x < j->x && i->y >= j->y || i->x >= j->x && i->y > j->y)
+                {
+                    pc.poly(*j, *i, false);
+                    pd.poly(*j, *i, true);
+                    ti = 0;
+                }
+                else
+                {
+                    pc.poly(*i, *j, false);
+                    pd.poly(*i, *j, true);
+                }
+                if (oc.count(pc) && oc.count(pd))
+                {
+                    if (ti)
+                        cout << *i << *j;
+                    else
+                        cout << "!" << *j << *i;
+                    cout << pc << pd << endl;
+                    ++ans;
+                }
+            }
+        cout << ans << endl;
     }
     return 0;
 }
