1308dec887978ce30b857d5e8e323d00a8627237
ctuu <ct.liuu@gmail.com>
2017-08-07 14:58:29 +0800

    commit

---
+++ Game/HDU/hdu1712.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define N 105
+#define M 105
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n
+    return 0;
+}
--- Game/HDU/hdu3535.cpp
+++ Game/HDU/hdu3535.cpp
@@ -13,6 +13,7 @@ struct no
 {
     int ci, gi;
     no() = default;
+    // no(){}
     no(int a, int b)
     {
         this->ci = a;
@@ -26,62 +27,38 @@ int main()
     int n, t;
     while (cin >> n >> t)
     {
-        int mt = t;
         array<vector<no>, N> va;
         array<array<int, N>, T> dp;
         for (auto &i : dp)
             i.fill(0);
         for (int i = 1; i <= n; ++i)
         {
-            int m, s, mm = INF;
+            int m, s;
             cin >> m >> s;
             for (int j = 0; j < m; ++j)
             {
                 int ci, gi;
                 cin >> ci >> gi;
                 va[i].push_back(no(ci, gi));
-                if (s == 0)
-                    mm = min(ci, mm);
             }
-            //0
             for (int j = 0; j <= t; ++j)
                 dp[i][j] = dp[i - 1][j];
             if (s == 0)
             {
-                mt -= mm;
                 for (int j = 0; j <= t; ++j)
                     dp[i][j] = -INF;
                 for (int j = 0; j < m; ++j)
                     for (int k = t; k >= va[i][j].ci; --k)
                         dp[i][k] = max(dp[i][k], max(dp[i][k - va[i][j].ci] + va[i][j].gi, dp[i - 1][k - va[i][j].ci] + va[i][j].gi));
             }
-            //1
-            else if (s == 1)
-            {
-                for (int j = 0; j < m; ++j)
-                    for (int k = t; k >= va[i][j].ci; --k)
-                        dp[i][k] = max(dp[i][k], dp[i - 1][k - va[i][j].ci] + va[i][j].gi);
-            }
-            //2
             else
             {
                 for (int j = 0; j < m; ++j)
                     for (int k = t; k >= va[i][j].ci; --k)
-                        dp[i][k] = max(dp[i][k], dp[i][k - va[i][j].ci] + va[i][j].gi);
+                        dp[i][k] = max(dp[i][k], dp[i - 2 + s][k - va[i][j].ci] + va[i][j].gi);
             }
         }
-        // if (mt < 0)
-        // {
-        //     cout << -1 << endl;
-        //     continue;
-        // }
-        for (int i = 0; i <= n; ++i)
-        {
-            for (int j = 0; j <= t; ++j)
-                cout << dp[i][j] << " ";
-            cout << endl;
-        }
-        cout << dp[n][t] << endl;
+        cout << max(dp[n][t], -1) << endl;
     }
     return 0;
 }
