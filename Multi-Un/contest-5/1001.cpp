c66362aeecf39426cfcfb2633e9e6146e06ae3e9
ctuu <ct.liuu@gmail.com>
2017-08-08 13:54:16 +0800

    commit

--- Multi-Un/contest-5/1001.cpp
+++ Multi-Un/contest-5/1001.cpp
@@ -6,37 +6,72 @@
 #define B 50005
 using namespace std;
 
+int PowerMod(int a, int b, int c)
+{
+    int ans = 1;
+    a = a % c;
+    while (b > 0)
+    {
+        if (b % 2 == 1)
+            ans = (ans * a) % c;
+        b = b / 2;
+        a = (a * a) % c;
+    }
+    return ans;
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    // ios_base::sync_with_stdio(0);
+    // cin.tie(0);
     int t;
-    array<int, N> ch;
+    // array<int, N> ch;
     array<int, M> ca;
     array<int, M> qr;
+    array<int, M> re;
+    array<int, 17> bin;
+    //1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536
     cin >> t;
     while (t--)
     {
+        bin.fill(0);
         int n, m, q;
         cin >> n >> m >> q;
+        cout << n << "!" << endl;
         for (int i = 0; i < n; ++i)
-            cin >> ch[i];
+        {
+            cout << i << endl;
+            int temp, r = 65536, j = 16;
+            cin >> temp;
+            while (temp)
+            {
+                if (temp >= r)
+                {
+                    temp -= r;
+                    ++bin[j];
+                }
+                r >>= 1;
+                --j;
+            }
+        }
+        // for (int i = 0; i < 16; ++i)
+        //     cout << bin[i] << " ";
+        // cout << endl;
         for (int i = 0; i < m; ++i)
             cin >> ca[i];
         for (int i = 0; i < q; ++i)
             cin >> qr[i];
-        for (int i = 0; i < q; ++i)
+        for (int j = 0; j < m; ++j)
         {
-            int ans = 0;
-            for (int j = 0; j < n; ++j)
-                for (int k = 0; k < m; ++k)
+            for (int k = 0; k < 16; ++k)
+                if (bin[k] && PowerMod(2, k, ca[j]) == qr[i])
                 {
-                    // cout << ch[j] << " " << ca[k] << " " << ch[j] % ca[k] << " " << qr[i] << endl;
-                    if (ch[j] % ca[k] == qr[i])
-                        ++ans;
+                    cout << k << " " << ca[j] << " " << qr[i] << endl;
+                    ans += bin[k];
                 }
-            cout << (ans & 1) << endl;
         }
+        cout << (ans & 1) << endl;
     }
-    return 0;
+}
+return 0;
 }
