df9ae79e84b80d605e21f175a9909d5add21d81b
ctuu <ct.liuu@gmail.com>
2017-08-10 15:03:32 +0800

    commit

--- Multi-Un/contest-6/1008.cpp
+++ Multi-Un/contest-6/1008.cpp
@@ -7,7 +7,7 @@
 #define M 5005
 #define S 5005
 using namespace std;
-array<array<int, S>, S> dp;
+array<int, S> dp;
 array<int, S> co;
 int main()
 {
@@ -21,27 +21,24 @@ int main()
         cin >> m;
         string s;
         cin >> s;
-        for (auto &i : dp)
-            i.fill(0);
-        // for (auto &i : co)
-
         int ct = s.size();
         int ans = 0;
         for (int i = 0; i < ct; ++i)
         {
             int ti = i, j = ct - 1;
             co.fill(0);
-            for (int k = j; k > co[j] + ti; --k)
+            dp.fill(0);
+            for (int k = j; k > ti + co[j]; --k)
             {
-                if (dp[i][j] + abs(s[ti + co[j]] - s[k]) <= m)
+                if (dp[j] + abs(s[ti + co[j]] - s[k]) <= m)
                 {
-                    dp[i][j] += abs(s[ti + co[j]] - s[k]);
+                    dp[j] += abs(s[ti + co[j]] - s[k]);
                     ++co[j];
                     ans = max(ans, co[j]);
                 }
                 else
                 {
-                    dp[i][j - 1] = dp[i][j] - abs(s[ti] - s[j]);
+                    dp[j - 1] = dp[j] - abs(s[ti] - s[j]);
                     co[j - 1] = co[j] - 1;
                     --j;
                     ++ti;
@@ -50,12 +47,27 @@ int main()
             }
         }
 
-        // for (int i = 0; i < ct; ++i)
+        // for (int i = ct - 1; i > -1; --i)
         // {
-        //     for (int j = 0; j < ct; ++j)
+        //     int ti = i, j = 0;
+        //     co.fill(0);
+        //     dp.fill(0);
+        //     for (int k = j; k < ti - co[j]; ++k)
         //     {
-        //         cout << co[i][j];
-        //         ans = max(ans, co[i][j]);
+        //         if (dp[j] + abs(s[ti - co[j]] - s[k]) <= m)
+        //         {
+        //             dp[j] += abs(s[ti - co[j]] - s[k]);
+        //             ++co[j];
+        //             ans = max(ans, co[j]);
+        //         }
+        //         else
+        //         {
+        //             dp[j + 1] = dp[j] - abs(s[ti] - s[j]);
+        //             co[j + 1] = co[j] - 1;
+        //             ++j;
+        //             --ti;
+        //             --k;
+        //         }
         //     }
         // }
         cout << ans << endl;
