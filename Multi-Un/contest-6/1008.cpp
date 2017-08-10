b201558375940c9a66c2fe6ba4fd603c033b0727
ctuu <ct.liuu@gmail.com>
2017-08-10 14:23:23 +0800

    commit

--- Multi-Un/contest-6/1008.cpp
+++ Multi-Un/contest-6/1008.cpp
@@ -8,7 +8,7 @@
 #define S 5005
 using namespace std;
 array<array<int, S>, S> dp;
-array<array<int, S>, S> co;
+array<int, S> co;
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -23,32 +23,33 @@ int main()
         cin >> s;
         for (auto &i : dp)
             i.fill(0);
-        for (auto &i : co)
-            i.fill(0);
+        // for (auto &i : co)
+
         int ct = s.size();
         int ans = 0;
         for (int i = 0; i < ct; ++i)
         {
             int ti = i, j = ct - 1;
-            for (int k = j; k > co[i][j] + ti; --k)
+            co.fill(0);
+            for (int k = j; k > co[j] + ti; --k)
             {
-                if (dp[i][j] + abs(s[ti + co[i][j]] - s[k]) <= m)
+                if (dp[i][j] + abs(s[ti + co[j]] - s[k]) <= m)
                 {
-                    dp[i][j] += abs(s[ti + co[i][j]] - s[k]);
-                    ++co[i][j];
-                    ans = max (ans, co[i][j]);
+                    dp[i][j] += abs(s[ti + co[j]] - s[k]);
+                    ++co[j];
+                    ans = max(ans, co[j]);
                 }
                 else
                 {
                     dp[i][j - 1] = dp[i][j] - abs(s[ti] - s[j]);
-                    co[i][j - 1] = co[i][j] - 1;
+                    co[j - 1] = co[j] - 1;
                     --j;
                     ++ti;
                     ++k;
                 }
             }
         }
-        
+
         // for (int i = 0; i < ct; ++i)
         // {
         //     for (int j = 0; j < ct; ++j)
