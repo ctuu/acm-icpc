ddaacd46130c10bc2102a70f116f4a78ebb39023
ctuu <ct.liuu@gmail.com>
2017-10-20 11:19:02 +0800

    ommit

--- ACM/CodeForces/828B.cpp
+++ ACM/CodeForces/828B.cpp
@@ -8,8 +8,9 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int n, m;
-    int t, b, l, r;
+    int t, b, l, r, sum = 0;
     char c;
+    bool hasb = 0;
     array<array<char, 103>, 103> ar;
     t = l = 199;
     b = r = -1;
@@ -21,30 +22,19 @@ int main()
             ar[i][j] = c;
             if (c == 'B')
             {
-                if (i < t)
-                    t = i;
-                if (j < l)
-                    l = j;
-                if (i > b)
-                    b = i;
-                if (j > r)
-                    r = j;
+                ++sum;
+                t = min(i, t);
+                l = min(j, l);
+                b = max(i, b);
+                r = max(j, r);
             }
         }
-        int len = min(min(n, m), max(b - t + 1, r - l + 1));
-        // cout << t << " " << b << " " << l << " " << r << " " << len << endl;
-    int ans = 0;
-    for (int i = b; i > b - len; --i)
-        for (int j = r; j > r - len; --j)
-            if (ar[i][j] == 'W')
-                ans++;
-    if (t == l && t == r && t == b)
-        cout << 0;
-    else if (t == 199 && t == l && r == -1 && r == b)
+    int len = max(b - t + 1, r - l + 1);
+    if (!sum)
         cout << 1;
-    else if (b - len > t || r - len > l)
+    else if (len > n || len > m)
         cout << -1;
     else
-        cout << ans;
+        cout << len * len - sum;
     return 0;
 }
