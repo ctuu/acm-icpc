1247c870a99add21d598b3ecbcffed81bc6a7580
ctuu <ct.liuu@gmail.com>
2017-10-31 23:46:17 +0800

    commit

--- ACM/HDU/hdu4786.cpp
+++ ACM/HDU/hdu4786.cpp
@@ -3,16 +3,16 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#define MN 100010
 using namespace std;
-#define G array<vector<Edge>, MN>
-const int MN = 1e5 + 7;
+#define G vector<vector<Edge>>
 int ct = 0;
 struct Edge
 {
     int v;
-    int c;
+    bool c;
     Edge() = default;
-    Edge(int v, int c)
+    Edge(int v, bool c)
     {
         this->v = v;
         this->c = c;
@@ -26,50 +26,43 @@ struct Edge
         return a.c < b.c;
     }
 };
-G gr;
 int mst(G &gr);
-int mst2(G &gr);
+int nmst(G &gr);
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int fib[35];
-    fib[0] = fib[1] = 1;
-    for (int i = 2; i <= 31; i++)
+    int fib[27];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i < 27; i++)
         fib[i] = fib[i - 2] + fib[i - 1];
     int T, n, m;
     cin >> T;
     for (int t = 1; t <= T; ++t)
     {
         cin >> n >> m;
-        for (int i = 0; i <= n; i++)
-            gr[i].clear();
-        // gr.resize(n);
+        G gr;
+        gr.resize(n);
         int u, v;
         bool c, fl = 0;
         for (int i = 0; i < m; ++i)
         {
             cin >> u >> v >> c;
-            gr[u].push_back(Edge(v, c));
-            gr[v].push_back(Edge(u, c));
+            gr[u - 1].push_back(Edge(v - 1, c));
+            gr[v - 1].push_back(Edge(u - 1, c));
         }
         ct = 1;
-        cout << "Case #" << t << ": ";
         int a1 = mst(gr);
-        if (ct != n)
-        {
-            cout << "No" << endl;
-            continue;
-        }
-        int a2 = mst2(gr);
-        // cout <<a1 << " " << a2<<endl;
-        for (int i = 1; i <= 33; ++i)
+        int a2 = nmst(gr);
+        cout << "Case #" << t << ": ";
+        for (int i = 1; i < 27; ++i)
             if (fib[i] <= a1 && fib[i] >= a2)
             {
                 fl = 1;
                 break;
             }
-        if (fl)
+        if (fl && ct == n)
             cout << "Yes" << endl;
         else
             cout << "No" << endl;
@@ -83,8 +76,8 @@ int mst(G &gr)
     priority_queue<Edge> pq;
     array<bool, MN> mrk;
     mrk.fill(0);
-    mrk[1] = 1;
-    for (auto e : gr[1])
+    mrk[0] = 1;
+    for (auto e : gr[0])
         pq.push(e);
     while (!pq.empty())
     {
@@ -103,15 +96,15 @@ int mst(G &gr)
     return sum;
 }
 
-int mst2(G &gr)
+int nmst(G &gr)
 {
     int sum = 0;
-    priority_queue<Edge> pq;
+    priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
     array<bool, MN> mrk;
     mrk.fill(0);
-    mrk[1] = 1;
-    for (auto e : gr[1])
-        pq.push(Edge(e.v, -e.c));
+    mrk[0] = 1;
+    for (auto e : gr[0])
+        pq.push(e);
     while (!pq.empty())
     {
         Edge e = pq.top();
@@ -119,11 +112,11 @@ int mst2(G &gr)
         int v = e.v;
         if (mrk[v])
             continue;
-        sum -= e.c;
+        sum += e.c;
         mrk[v] = 1;
         for (auto e : gr[v])
             if (!mrk[e.v])
-                pq.push(Edge(e.v, -e.c));
+                pq.push(e);
     }
     return sum;
 }
--- ACM/HDU/hdu4786test.cpp
+++ ACM/HDU/hdu4786test.cpp
@@ -3,184 +3,126 @@
 #include <iostream>
 #include <queue>
 #include <vector>
-#define MV 100030
+#define MN 100010
 using namespace std;
+#define G vector<vector<Edge>>
 int ct = 0;
-int ct2 = 0;
-class Edge
+struct Edge
 {
-  private:
-    int v, w;
-    int wei;
-
-  public:
-    Edge(int v, int w, int wei)
+    int v;
+    int c;
+    Edge() = default;
+    Edge(int v, int c)
     {
         this->v = v;
-        this->w = w;
-        this->wei = wei;
-    }
-    int either()
-    {
-        return v;
-    }
-    int other(int v)
-    {
-        if (v == this->v)
-            return w;
-        else
-            return v;
-    }
-    int weight()
-    {
-        return wei;
+        this->c = c;
     }
     friend bool operator>(const Edge &a, const Edge &b)
     {
-        return a.wei > b.wei;
+        return a.c > b.c;
     }
     friend bool operator<(const Edge &a, const Edge &b)
     {
-        return a.wei < b.wei;
+        return a.c < b.c;
     }
 };
-class Wgraph
-{
-  private:
-    int V, E;
-    vector<vector<Edge>> gr;
-
-  public:
-    Wgraph(int V)
-    {
-        gr.resize(V);
-    }
-    void adde(int v, int w, int wei)
-    {
-        gr[v].push_back(Edge(v, w, wei));
-    }
-    vector<Edge> adj(int v)
-    {
-        return gr[v];
-    }
-};
-class Prim
-{
-  private:
-    priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
-    array<bool, MV> mrk;
-    queue<Edge> mst;
-    void visit(Wgraph gr, int v)
-    {
-        ++ct;
-        mrk[v] = 1;
-        for (auto e : gr.adj(v))
-        {
-            if (!mrk[e.other(v)])
-                pq.push(e);
-        }
-    }
-
-  public:
-    long long calc(Wgraph gr)
-    {
-        int sum = 0;
-        mrk.fill(0);
-        visit(gr, 0);
-        while (!pq.empty())
-        {
-            Edge e = pq.top();
-            pq.pop();
-            int v = e.either(), w = e.other(v);
-            if (mrk[v] && mrk[w])
-                continue;
-            mst.push(e);
-            sum += e.weight();
-            if (!mrk[v])
-                visit(gr, v);
-            if (!mrk[w])
-                visit(gr, w);
-        }
-        return sum;
-    }
-};
-class Prim2
-{
-  private:
-    priority_queue<Edge> pq;
-    array<bool, MV> mrk;
-    queue<Edge> mst;
-    void visit(Wgraph gr, int v)
-    {
-        ++ct2;
-        mrk[v] = 1;
-        for (auto e : gr.adj(v))
-        {
-            if (!mrk[e.other(v)])
-                pq.push(e);
-        }
-    }
-
-  public:
-    long long calc(Wgraph gr)
-    {
-        int sum = 0;
-        mrk.fill(0);
-        visit(gr, 0);
-        while (!pq.empty())
-        {
-            Edge e = pq.top();
-            pq.pop();
-            int v = e.either(), w = e.other(v);
-            if (mrk[v] && mrk[w])
-                continue;
-            mst.push(e);
-            sum += e.weight();
-            if (!mrk[v])
-                visit(gr, v);
-            if (!mrk[w])
-                visit(gr, w);
-        }
-        return sum;
-    }
-};
+int mst(G &gr);
+int nmst(G &gr);
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int fibo[35];
-    fibo[0]=0;fibo[1]=1;
-    for(int i=2;i<35;i++) fibo[i]=fibo[i-1]+fibo[i-2];
+    int fib[35];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i <= 31; i++)
+        fib[i] = fib[i - 2] + fib[i - 1];
     int T, n, m;
     cin >> T;
     for (int t = 1; t <= T; ++t)
     {
         cin >> n >> m;
-        Wgraph gr = Wgraph(n);
+        G gr;
+        gr.resize(n);
         int u, v, c;
+        bool fl = 0;
         for (int i = 0; i < m; ++i)
         {
             cin >> u >> v >> c;
-            gr.adde(u - 1, v - 1, c);
+            gr[u - 1].push_back(Edge(v - 1, c));
+            gr[v - 1].push_back(Edge(u - 1, c));
         }
-        bool fl = 0;
-        ct = 0;
-        ct2 = 0;
-        Prim prim;
-        Prim2 prim2;
-        int ano = prim.calc(gr);
-        int ant = prim2.calc(gr);
+        ct = 1;
+        int a1 = mst(gr);
+        int a2 = nmst(gr);
         cout << "Case #" << t << ": ";
-        for (int i = 1; i <= 33; ++i)
-            if (fibo[i] <= ant && fibo[i] >= ano)
+        for (int i = 1; i <= 31; ++i)
+            if (fib[i] <= a1 && fib[i] >= a2)
             {
                 fl = 1;
                 break;
             }
-        if (fl && ct == ct2 && ct == n)
+        if (fl && ct == n)
             cout << "Yes" << endl;
         else
             cout << "No" << endl;
     }
     return 0;
+}
+
+int mst(G &gr)
+{
+    int sum = 0;
+    priority_queue<Edge> pq;
+    array<bool, MN> mrk;
+    mrk.fill(0);
+    mrk[0] = 1;
+    for (auto e : gr[0])
+        pq.push(e);
+    while (!pq.empty())
+    {
+        Edge e = pq.top();
+        pq.pop();
+        int v = e.v;
+        if (mrk[v])
+            continue;
+        sum += e.c;
+        if (!mrk[v])
+        {
+            ++ct;
+            mrk[v] = 1;
+            for (auto e : gr[v])
+                if (!mrk[e.v])
+                    pq.push(e);
+        }
+    }
+    return sum;
+}
+
+int nmst(G &gr)
+{
+    int sum = 0;
+    priority_queue<Edge> pq;
+    array<bool, MN> mrk;
+    mrk.fill(0);
+    mrk[0] = 1;
+    for (auto e : gr[0])
+        pq.push(Edge(e.v, -e.c));
+    while (!pq.empty())
+    {
+        Edge e = pq.top();
+        pq.pop();
+        int v = e.v;
+        if (mrk[v])
+            continue;
+        sum -= e.c;
+        if (!mrk[v])
+        {
+            mrk[v] = 1;
+            for (auto e : gr[v])
+                if (!mrk[e.v])
+                    pq.push(Edge(e.v, -e.c));
+        }
+    }
+    return sum;
 }
