c06b0d05b649eb4a2d66d128b6245764f7d6eabd
ctuu <ct.liuu@gmail.com>
2017-11-21 22:04:11 +0800

    commit

--- ACM/HDU/hdu2846.cpp
+++ ACM/HDU/hdu2846.cpp
@@ -1,19 +1,25 @@
 #include <algorithm>
 #include <array>
+// #include <cstdio>
 #include <cstring>
-#include <cstdio>
-// #include <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 const int CHARSET = 26, BASE = 'a', MAX_NODE = 200005;
 array<bool, MAX_NODE> ist;
 struct Trie
 {
-    int tot, root, child[MAX_NODE][CHARSET];
+    int tot, root;
+    // int child[MAX_NODE][CHARSET];
+    vector<array<int, CHARSET>> child;
     int flag[MAX_NODE];
     Trie()
     {
-        memset(child[1], 0, sizeof(child[1]));
+        // memset(child[1], 0, sizeof(child[1]));
+        array<int, CHARSET> tmp;
+        tmp.fill(0);
+        child.push_back(tmp);
+        child.push_back(tmp);
         flag[1] = 0;
         root = tot = 1;
     }
@@ -26,9 +32,15 @@ struct Trie
             if (*cur == 0)
             {
                 *cur = ++tot;
-                memset(child[tot], 0, sizeof(child[tot]));
+                array<int, CHARSET> tmp;
+                child.push_back(tmp);
+                // printf("%s  %d %d\n",p, tot,child.size());
+                // cout << p << " "<< tot << " " << child.size() << endl;
+                child[tot].fill(0);
+                // memset(child[tot], 0, sizeof(child[tot]));
                 flag[tot] = 0;
             }
+            // printf("!!!\n");
             if (!ist[*cur])
                 ++flag[*cur];
             ist[*cur] = 1;
@@ -46,23 +58,30 @@ struct Trie
 Trie e;
 int main()
 {
-    // ios_base::sync_with_stdio(0);
-    // cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     int n;
     string s;
+    // char s[23];
     cin >> n;
+    // scanf("%d", &n);
     for (int t = 0; t < n; ++t)
     {
         ist.fill(0);
         cin >> s;
+        // scanf("%s", s);
+        // int si = strlen(s);
         int si = s.size();
         for (int i = 0; i < si; ++i)
             e.insert(s.substr(i, si).c_str(), t);
     }
+    // scanf("%d", &n);
     cin >> n;
     for (int t = 0; t < n; ++t)
     {
         cin >> s;
+        // scanf("%s", s);
+        // printf("%d\n", e.query(s));
         cout << e.query(s.c_str()) << endl;
     }
     return 0;
--- temp.cpp
+++ temp.cpp
@@ -3,14 +3,26 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+struct test
+{
+    vector<array<int, 26>> child;
+
+    void cr()
+    {
+        array<int, 26> a;
+        child.push_back(a);
+    }
+};
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    string s;
-    cin >> s;
-    int si = s.size();
-    for (int i = 0; i < si; ++i)
-        cout << s.substr(i, si) << endl;
+    test e;
+    array<int, 26> b;
+    e.child.push_back(b);
+    cout << e.child.size() << endl;
+    e.cr();
+    cout << e.child.size() << endl;
     return 0;
 }
