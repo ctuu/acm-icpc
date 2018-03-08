462c783be186b6ac291423c9375e5b25ad5a5f72
ctuu <ct.liuu@gmail.com>
2018-03-08 09:49:46 +0800

    change folders

--- Problems/HDU/hdu2612.cpp
+++ Problems/HDU/hdu2612.cpp
@@ -4,6 +4,7 @@
 #include <queue>
 #include <vector>
 using namespace std;
+const int N = 220;
 const int INF = 0x3f3f3f3f;
 struct Po
 {
@@ -15,14 +16,17 @@ int main()
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     int n, m;
-    char mz[220][220];
-    bool vn[220][220], vm[220][220];
-    int sn[220][220];
+    char mz[N][N];
+    bool vn[N][N], vm[N][N];
+    int sn[N][N];
     while (cin >> n >> m)
     {
-        memset(vn, 0, sizeof(vn));
-        memset(vm, 0, sizeof(vm));
-        memset(sn, INF, sizeof(sn));
+        for(int i = 0; i< N; ++i)
+            for(int j = 0; j < N; ++j)
+            {
+                vn[i][j] = vm[i][j] = 0;
+                sn[i][j] = INF;
+            }
         queue<Po> fn, fm;
         int ans = INF;
         for (int i = 0; i < n; ++i)
@@ -32,13 +36,15 @@ int main()
                 if (mz[i][j] == 'Y')
                 {
                     fn.push(Po(i, j, 0));
-                    vn[i][j] = vm[i][j] = 1;
+                    vn[i][j] = 1;
                 }
                 if (mz[i][j] == 'M')
                 {
                     fm.push(Po(i, j, 0));
-                    vm[i][j] = vn[i][j] = 1;
+                    vm[i][j] = 1;
                 }
+                if(mz[i][j] == '#')
+                    vm[i][j] = vn[i][j] = 1;
             }
         while (!fn.empty())
         {
@@ -46,14 +52,11 @@ int main()
             fn.pop();
             int &cx = cu.x, &cy = cu.y, &cs = cu.s;
             if (mz[cx][cy] == '@')
-            {
                 sn[cx][cy] = min(sn[cx][cy],cs);
-                continue;
-            }
             int t[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
             for (auto i : t)
                 if (cx + i[0] < n && cx + i[0] > -1 && cy + i[1] < m && cy + i[1] > -1)
-                    if (!vn[cx + i[0]][cy + i[1]] && mz[cx + i[0]][cy + i[1]] != '#')
+                    if (!vn[cx + i[0]][cy + i[1]])
                     {
                         fn.push(Po(cx + i[0], cy + i[1], cs + 1));
                         vn[cx + i[0]][cy + i[1]] = 1;
@@ -64,15 +67,12 @@ int main()
             Po cu = fm.front();
             fm.pop();
             int &cx = cu.x, &cy = cu.y, &cs = cu.s;
-            if (mz[cx][cy] == '@' && vn[cx][cy])
-            {
+            if (mz[cx][cy] == '@')
                 ans = min(ans, sn[cx][cy] + cs);
-                continue;
-            }
             int t[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
             for (auto i : t)
                 if (cx + i[0] < n && cx + i[0] > -1 && cy + i[1] < m && cy + i[1] > -1)
-                    if (!vm[cx + i[0]][cy + i[1]] && mz[cx + i[0]][cy + i[1]] != '#')
+                    if (!vm[cx + i[0]][cy + i[1]])
                     {
                         fm.push(Po(cx + i[0], cy + i[1], cs + 1));
                         vm[cx + i[0]][cy + i[1]] = 1;
