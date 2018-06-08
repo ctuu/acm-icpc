a90025a7c1e725d69924e765aa0a186be094a7af
ctuu <ct.liuu@gmail.com>
2018-06-08 15:44:50 +0800

    commit

--- Problems/CodeForces/R#486D3/e.cpp
+++ Problems/CodeForces/R#486D3/e.cpp
@@ -3,34 +3,42 @@
 #include <iostream>
 using namespace std;
 const int INF = 0x3f3f3f3f;
+
+int deal(string s, int a, int b)
+{
+    int tns = a + b;
+    string tms = s.substr(a,1) + s.substr(b,1);
+    if(tms == "00" || tms == "57" || tms == "52" || tms == "05")
+        --tns;
+    if (s[a] > s[b])
+        swap(a, b);
+    tms = s.substr(a,1) + s.substr(b,1);
+    if(tms == "00" || tms == "25" || tms == "05" || tms == "57")
+    {
+        s.erase(max(a,b), 1);
+        s.erase(min(a,b), 1);
+        int si = s.size() - 1;
+        while (s[si] == '0' && si > 0)
+            si--;
+        tns += s.size() - 1 - si;
+        if (si == 0 && s[si] == '0')
+            return INF;
+        return tns;
+    }
+    return INF;
+}
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    array<int, 10> chs;
-    int zs = INF;
-    chs.fill(INF);
     string s;
     cin >> s;
     reverse(s.begin(), s.end());
     int si = s.size(), ans = INF;
     for (int i = 0; i < si; ++i)
-    {
-        if (s[i] == '0')
-        {
-            if (chs[0] < i)
-                zs = min(i, zs);
-            else
-                chs[0] = i;
-            continue;
-        }
-        chs[s[i] - '0'] = min(i, chs[s[i] - '0']);
-    }
-    ans = min(ans, chs[5] + chs[7] + (chs[7] > chs[5] ? -1 : 0));
-    ans = min(ans, chs[2] + chs[5] + (chs[2] > chs[5] ? -1 : 0));
-    ans = min(ans, zs + chs[0] - 1);
-    ans = min(ans, chs[0] + chs[5] + (chs[5] > chs[0] ? -1 : 0));
+        for (int j = i + 1; j < si; ++j)
+            ans = min(ans, deal(s, i, j));
     ans = ans >= INF ? -1 : ans;
     cout << ans << endl;
     return 0;
